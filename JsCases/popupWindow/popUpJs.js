18d1356423f5c8e39f044094984d0e03cbbc419d
jiaodi0033 <2322009884@qq.com>
2020-11-13 08:28:41 +0800

    feat:add JsCases

---
+++ JsCases/popupWindow/popUpJs.js
@@ -0,0 +1,12 @@
+$('.openBtn').click(function () {
+    $('.modal').show();
+})
+$('.closeBtn').click(function () {
+    $('.modal').hide();
+})
+let modal=document.querySelector('.modal');
+window.onclick=function (event) {
+    if(event.target == modal){
+        $('.modal').hide();
+    }
+}
